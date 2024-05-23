da24824e75370feebae905395a9aaeac4a0468be
Nicolás Van Sint Jan <nicovsj@proton.me>
2024-05-23 13:33:32 +0200

    ♻️ fix: Include missing header file in main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,13 @@
+#include <fstream>
 #include <string>
 #include <vector>
 
-#include <antlr4-runtime.h>
-
+#include "antlr4-runtime.h"
 
 int main(int argc, const char *argv[]) {
   std::vector<std::string> msg{"Hello", "C++",     "World",
                                "from",  "VS Code", "and the C++ extension!"};
-  
+
   std::ifstream stream;
 
   stream.open("example.txt");
