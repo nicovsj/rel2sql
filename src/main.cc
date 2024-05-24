914645d1c6e0e6e0bf032d531a1c91d292bad9d5
Nicolás Van Sint Jan <nicovsj@proton.me>
2024-05-24 19:36:22 +0200

    ♻️ Update example main file

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,10 @@
 #include <fstream>
-#include <string>
-#include <vector>
 
 #include "antlr4-runtime.h"
+#include "RelLexer.h"
+#include "RelParser.h"
 
 int main(int argc, const char *argv[]) {
-  std::vector<std::string> msg{"Hello", "C++",     "World",
-                               "from",  "VS Code", "and the C++ extension!"};
-
   std::ifstream stream;
 
   stream.open("example.txt");
