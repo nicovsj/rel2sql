35588338d53771bcfe770b26f96f76346df5423a
Nicolás Van Sint Jan <nicovsj@proton.me>
2024-06-15 01:14:02 +0200

    ✅ Test var_lists function

--- tests/test_translation.cc
+++ tests/test_translation.cc
@@ -24,5 +24,31 @@ TEST(TranslationUtilityFunctionsTest, EqualitySS) {
 
   os << *condition;
 
-  EXPECT_EQ(os.str(), "F.x = G.x");
+  EXPECT_EQ(os.str(), "G.x = F.x");
+}
+
+TEST(TranslationUtilityFunctionsTest, VarListSS) {
+  std::string input = "F(x) and G(x, y)";
+
+  auto parser = rel_parser::GetParser(input);
+
+  auto tree = dynamic_cast<rel_parser::PrunedCoreRelParser::BinOpContext*>(parser->formula());
+
+  auto ast = rel_parser::GetExtendedASTFromTree(tree);
+
+  auto table_F = std::make_shared<Table>("F");
+  auto table_G = std::make_shared<Table>("G");
+
+  auto var_list =
+      VarListSS(std::unordered_map<antlr4::ParserRuleContext*, std::shared_ptr<Source>>{{tree->lhs, table_F},
+                                                                                        {tree->rhs, table_G}},
+                *ast.extended_data);
+
+  std::ostringstream os;
+
+  for (auto& col : var_list) {
+    os << *col << " ";
+  }
+
+  EXPECT_EQ(os.str(), "G.x G.y ");
 }
