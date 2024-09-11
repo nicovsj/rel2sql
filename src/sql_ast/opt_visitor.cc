32a502d7776e9fd688c67b416c18878b05ac9137
Nicolás Van Sint Jan <nicovsj@proton.me>
2024-09-11 07:08:04 -0300

    📝 Add docstring for optimization

--- src/sql_ast/opt_visitor.cc
+++ src/sql_ast/opt_visitor.cc
@@ -7,6 +7,16 @@ namespace sql::ast {
 void OptimizerVisitor::Visit(Expression& expr) { expr.Accept(*this); }
 
 bool OptimizerVisitor::TryReplaceConstantInWhere(const std::shared_ptr<Source>& source, FromStatement& from_statement) {
+  /**
+   * Tries to replace a constant in the WHERE condition of a FROM statement.
+   *
+   * This function checks if the given source is a SelectStatement with a single column that is a constant.
+   * If so, it replaces the constant in the WHERE condition of the given FROM statement.
+   *
+   * @param source The source to check and potentially replace the constant from.
+   * @param from_statement The FROM statement whose WHERE condition may be modified.
+   * @return true if a constant was successfully replaced in the WHERE condition, false otherwise.
+   */
   if (auto select = std::dynamic_pointer_cast<SelectStatement>(source->sourceable)) {
     if (select->columns.size() == 1) {
       if (auto term_selectable = std::dynamic_pointer_cast<TermSelectable>(select->columns[0])) {
