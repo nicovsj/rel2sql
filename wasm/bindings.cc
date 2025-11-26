f2ca39793d042c26219f0262760cef729e688153
Nicolás Van Sint Jan <nicovsj@pm.me>
2025-11-26 11:18:12 -0300

    🐛 Fix wrong include in bindings.cc

--- wasm/bindings.cc
+++ wasm/bindings.cc
@@ -9,7 +9,7 @@
 
 // Include the actual rel2sql headers
 #include "rel2sql/rel2sql.h"
-#include "rel_ast/edb_info.h"
+#include "rel_ast/relation_info.h"
 
 // WebAssembly wrapper functions
 std::string translate_rel2sql(const std::string& input) { return rel2sql::Translate(input); }
